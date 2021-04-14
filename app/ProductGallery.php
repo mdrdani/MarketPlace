3efb430214ef89718bba04a8f47a3e6ae4eb9af6
mdrdani <sudokill3004@gmail.com>
2021-04-14 10:11:59 +0700

    Make Model and Form Request Galery

--- app/ProductGallery.php
+++ app/ProductGallery.php
@@ -7,4 +7,16 @@ use Illuminate\Database\Eloquent\Model;
 class ProductGallery extends Model
 {
     //
+    protected $fillable = [
+        'photos' , 'products_id'
+    ];
+
+    protected $hidden =  [
+
+    ];
+
+    public function products()
+    {
+        return $this->belongsTo(Product::class,'products_id','id');
+    }
 }
