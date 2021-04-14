cbcb81ceb4d84cc2f91e0be7b80e643abe45dd10
mdrdani <sudokill3004@gmail.com>
2021-04-14 14:56:41 +0700

    Integrasi Landing Page Category & Product

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -1,6 +1,8 @@
 <?php
 
 namespace App\Http\Controllers;
+use App\Category;
+use App\Product;
 
 use Illuminate\Http\Request;
 
@@ -15,6 +17,11 @@ class HomeController extends Controller
      */
     public function index()
     {
-        return view('pages.home');
+        $categories = Category::take(6)->get();
+        $products = Product::with('galleries')->take(8)->get();
+        return view('pages.home', [
+            'categories' => $categories,
+            'products' => $products
+        ]);
     }
 }
--- routes/web.php
+++ routes/web.php
@@ -13,8 +13,12 @@ use Illuminate\Support\Facades\Route;
 |
 */
 Route::get('/', 'HomeController@index')->name('home');
+
 Route::get('/categories', 'CategoryController@index')->name('categories');
+Route::get('/categories/{id}', 'CategoryController@detail')->name('categories-detail');
+
 Route::get('/details/{id}', 'DetailController@index')->name('detail');
+
 Route::get('/cart', 'CartController@index')->name('cart');
 Route::get('/success', 'CartController@success')->name('success');
 Route::get('/register/success', 'Auth\RegisterController@success')->name('register-success');
@@ -34,6 +38,7 @@ Route::get('/dashboard/transactions/{id}', 'DashboardTransactionController@detai
 // Controller Setting and Account
 Route::get('/dashboard/settings', 'DashboardSettingController@store')->name('dashboard-settings-store');
 Route::get('/dashboard/account', 'DashboardSettingController@account')->name('dashboard-settings-account');
+
 
 // ->middleware(['auth','admin'])
 Route::prefix('admin')
