d5a1335103619f539415986356931e409186b27c
mdrdani <sudokill3004@gmail.com>
2021-05-02 20:22:50 +0700

    List Cart, Delete Product Cart, and Install Library Indoregion

--- routes/web.php
+++ routes/web.php
@@ -21,7 +21,11 @@ Route::get('/categories/{id}', 'CategoryController@detail')->name('categories-de
 Route::get('/details/{id}', 'DetailController@index')->name('detail');
 Route::post('/details/{id}', 'DetailController@add')->name('detail-add');
 
+// Cart
 Route::get('/cart', 'CartController@index')->name('cart');
+Route::delete('/cart/{id}', 'CartController@delete')->name('cart-delete');
+
+
 Route::get('/success', 'CartController@success')->name('success');
 Route::get('/register/success', 'Auth\RegisterController@success')->name('register-success');
 
