fd1f3b6418571b39204e62a42bf7ccf01a1010ef
mdrdani <sudokill3004@gmail.com>
2021-05-11 11:25:25 +0700

    Config Library Midtrans and modified model Transaction

--- app/Transaction.php
+++ app/Transaction.php
@@ -7,4 +7,15 @@ use Illuminate\Database\Eloquent\Model;
 class Transaction extends Model
 {
     //
+     protected $fillable = [
+        'users_id',
+        'insurance_price',
+        'shipping_price',
+        'total_price',
+        'code'
+    ];
+
+    protected $hidden = [
+
+    ]; 
 }
